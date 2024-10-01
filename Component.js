c433403b166760305330a56d7b3b0d9bf54099fc
ashish soni <ashishsoni4747@gmail.com>
2024-10-01 09:09:33 +0000

    test

--- Component.js
+++ Component.js
@@ -2,7 +2,7 @@ sap.ui.define([
 	"sap/ui/core/UIComponent"
 ], function (UIComponent) {
 	"use strict";
-
+// comment
 	var Component = UIComponent.extend("my.component.sample.cardContent.Component");
 	return Component;
 });
